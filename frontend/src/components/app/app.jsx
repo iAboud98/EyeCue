70def2a8b4f8518ad3c39610f9a9e22654075419
iAboud98 <aboudfialah98@gmail.com>
2025-08-09 16:16:37 +0300

    Webcam integration and Setup

---
+++ frontend/src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import CameraCapture from "../cameraCapture/cameraCapture";
+
+const  App= () => {
+  return (
+    <div className="App">
+      <CameraCapture/>
+    </div>
+  );
+}
+
+export default App;
